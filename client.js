cec54121b329ad18d7207dd461ccdf1ad01b2cb7
Jeffrey Meyer <itotallyrock@users.noreply.github.com>
2016-05-09 00:18:34 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,16 +1,22 @@
-var bets = []; //Append new bets
 var settings = {
-    debug: true,
-    basebet: 8,
+    debug: false,
+    basebet: parseInt(prompt("Basebet", "25")),
     stopcount: -1,
     stopbet: 600,
     greenskip: 2,
 };
 var getBetString = function() {
-    initBets();
     var r = "";
-    for (var i = 10; i > 0; i--)
-        r += bets[bets.length - i].toUpperCase();
+    $(".ball").each(function(i, e) {
+        var n = parseInt($(e).text());
+        if (n == 0) {
+            r += "G"
+        } else if (n > 0 && n < 8) {
+            r += "R";
+        } else if (n > 7 && n < 15) {
+            r += "B";
+        }
+    });
     return r;
 };
 var oppositeRoll = function(roll) {
@@ -20,18 +26,11 @@ var oppositeRoll = function(roll) {
 };
 var isRainbow = function() {
     var r = false;
-	// if (getBetString()[9] = getBetString()[7] && getBetString()[8] == getBetString()[6] && getBetString()[8] ==  oppositeRoll(getBetString()[9])) {
-	// 	r = true;
-	// }
-	if (getBetString().substring(6,9) == "RBRB" || getBetString().substring(6,9) == "BRBR") {
-		r = true;
-	}
-    // if (getBetString()[9] == oppositeRoll(getBetString()[8]) && getBetString()[8] == oppositeRoll(getBetString()[6])) {
-    //     r = true;
-    // }
+    if (getBetString().substring(7, 10) == "BRB" || getBetString().substring(7, 10) == "RBR") {
+        r = true;
+    }
     return r;
 };
-
 var isTrain = function() {
     var r = false;
     if (getBetString()[9] == getBetString()[8]) {
@@ -39,11 +38,9 @@ var isTrain = function() {
     }
     return r;
 };
-
-var afterRoll = function(roll) {
-    addBet($(".ball").last());
-};
-
+var trainLength = function() {
+    return getBetString().replace(/([RGB])\1+$/, "T").replace(/[RGB]/g, "").length + 1;
+}
 var predict = function() {
     var r = " ";
     if (isRainbow()) {
@@ -53,96 +50,55 @@ var predict = function() {
     } else {
         r = "?";
     }
-    if (getBetString()[9] == "G" || getBetString()[8] == "G") {
-        r = "S";
+    for (var i = 0; i < settings.greenskip; i++) {
+        if (getBetString()[9 - i] == "G" || getBetString()[8 - i] == "G") {
+            r = "S";
+        }
     }
     return r;
 };
+var getBetValue = function() {
+    var bet = (trainLength() - 1) * Math.floor(settings.basebet / 4) + settings.basebet;
 
-var initBets = function() {
-    $(".ball").each(function(i, e) {
-        addBet(e);
-    });
+    return bet;
 };
 
-var addBet = function(e) {
-    var n = parseInt($(e).text());
-    if (n == 0) {
-        bets[bets.length] = "G"
-    } else if (n > 0 && n < 8) {
-        bets[bets.length] = "R";
-    } else if (n > 7 && n < 15) {
-        bets[bets.length] = "B";
+var placeBet = function() {
+    if (settings.debug == true) console.log(predict());
+    var b = predict();
+    $("#betAmount").val(parseInt(getBetValue()).toString());
+    if (b == "B") {
+        $("[data-upper=14]").click(); //Black
+        $(".navbar").css("background-color", "#444")
+    } else if (b == "R") {
+        $("[data-upper=7]").click(); //R
+        $(".navbar").css("background-color", "#ff0055")
+    } else if (b == "G") {
+        $("[data-upper=0]").click(); //G
+        $(".navbar").css("background-color", "#449d44")
+    } else if (b == "?" || b == "S") {
+        $(".navbar").css("background-color", "#ffa500");
+    } else {
+        //Something unexpected predicted
     }
-}
-
-var init = function() {
-    $('#mainpage').prepend('<div class="predictme">' +
-        '<div class="predict-btn btn btn-inverse">Predict</div>' +
-        '<div class="ball ball-8">#</div>' +
-        '</div>');
+    if (settings.debug == true) console.log("total = " + total);
 };
-
-$('.predictme predict').on('click', function(e) {
-    console.log('clicked predict');
-    getNextBet();
-});
-
-var getNextBet = function() {
-    bets = [];
-    initBets();
-    // $(".predictme .ball").text(predict());
-    // if (predict() == "R") {
-    //     $(".predictme .ball").removeClass('ball-8');
-    //     $(".predictme .ball").addClass('ball-1');
-    // } else if (predict() == "B") {
-    //     $(".predictme .ball").removeClass('ball-1');
-    //     $(".predictme .ball").addClass('ball-8');
-    // } else {
-    // 	$(".predictme .ball").removeClass('ball-1');
-    // 	$(".predictme .ball").removeClass('ball-8');
-    // }
-    return predict();
-}
-
-init();
-// var clicked = false;
 
 var loop = setInterval(function() {
-    if (settings.debug == true) console.log(predict());
-    $("#betAmount").val(parseInt(settings.basebet).toString());
-    var b = getNextBet();
 
-	var total = 0;
+    var open = ($("#banner").text().indexOf("Rolling") > -1);
+    var rolled = ($("#banner").text().indexOf("CSGO") > -1);
+    var confirming = ($("#banner").text().indexOf("Confirming") > -1);
+    var rolling = ($("#banner").text().indexOf("***R") > -1);
+
+    var total = 0;
     $(".mytotal").each(function(i, o) {
         total += parseInt($(o).text());
     });
 
-	if (total >= settings.basebet) {
-        // clicked == true;
-    } else if (total == 0) {
-        // clicked = false;
-		if (b == "B") {
-	        $("[data-upper=14]").click(); //Black
-	        // clicked = true;
-			$("#mainpage").css("background-color", "black")
-	    } else if (b == "R") {
-	        $("[data-upper=7]").click(); //R
-	        // clicked = true;
-			$("#mainpage").css("background-color", "red")
-	    } else if (b == "G") {
-	        $("[data-upper=0]").click(); //G
-	        // clicked = true;
-			$("#mainpage").css("background-color", "green")
-	    } else if (b == "?" || b == "S") {
-			$("#mainpage").css("background-color", "orange");
-	        // clicked = true;
-	    } else {
-	        // clicked = false;
-	    }
+    if (open && !(total >= settings.basebet)) {
+        placeBet();
     }
-
-    if (settings.debug == true) console.log("total = " + total);
 }, 1000);
 
 window.onbeforeunload = function() {
